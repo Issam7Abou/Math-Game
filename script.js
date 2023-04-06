559d620ac085a7530319ca4eb2a7f4622285bf20
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-04-06 15:22:32 +0100

    Add files via upload
    added UI;
    added some event listeners to start the interactivity between pages of the game;
    added a countdown right before starting the game;
    added some of the logic behind the math questions;

---
+++ script.js
@@ -0,0 +1,146 @@
+// Pages
+const gamePage = document.getElementById('game-page');
+const scorePage = document.getElementById('score-page');
+const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
+// Splash Page
+const startForm = document.getElementById('start-form');
+const radioContainers = document.querySelectorAll('.radio-container');
+const radioInputs = document.querySelectorAll('input');
+const bestScores = document.querySelectorAll('.best-score-value');
+// Countdown Page
+const countdown = document.querySelector('.countdown');
+// Game Page
+const itemContainer = document.querySelector('.item-container');
+// Score Page
+const finalTimeEl = document.querySelector('.final-time');
+const baseTimeEl = document.querySelector('.base-time');
+const penaltyTimeEl = document.querySelector('.penalty-time');
+const playAgainBtn = document.querySelector('.play-again');
+
+// Equations
+let questionAmount = 0;
+let equationsArray = [];
+
+// Game Page
+let firstNumber = 0;
+let secondNumber = 0;
+let equationObject = {};
+const wrongFormat = [];
+
+// Time
+
+// Scroll
+
+// Get Random Number up toa max number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Create Correct/Incorrect Random Equations
+function createEquations() {
+  // Randomly choose how many correct equations there should be
+  const correctEquations = getRandomInt(questionAmount);
+  console.log('correct equations:', correctEquations);
+  // Set amount of wrong equations
+  const wrongEquations = questionAmount - correctEquations;
+  console.log('wrong equations:', wrongEquations);
+  // Loop through, multiply random numbers up to 9, push to array
+  for (let i = 0; i < correctEquations; i++) {
+    firstNumber = 
+    secondNumber = 
+    const equationValue = firstNumber * secondNumber;
+    const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
+    equationObject = { value: equation, evaluated: 'true' };
+    equationsArray.push(equationObject);
+  } 
+  // Loop through, mess with the equation results, push to array
+  for (let i = 0; i < wrongEquations; i++) {
+    firstNumber = 
+    secondNumber = 
+    const equationValue = firstNumber * secondNumber;
+    wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+    wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
+    const formatChoice = 
+    const equation = wrongFormat[formatChoice];
+    equationObject = { value: equation, evaluated: 'false' };
+    equationsArray.push(equationObject);
+  } 
+}
+
+// Dynamically adding correct/incorrect equations
+// function populateGamePage() {
+//   // Reset DOM, Set Blank Space Above
+//   itemContainer.textContent = '';
+//   // Spacer
+//   const topSpacer = document.createElement('div');
+//   topSpacer.classList.add('height-240');
+//   // Selected Item
+//   const selectedItem = document.createElement('div');
+//   selectedItem.classList.add('selected-item');
+//   // Append
+//   itemContainer.append(topSpacer, selectedItem);
+
+//   // Create Equations, Build Elements in DOM
+
+//   // Set Blank Space Below
+//   const bottomSpacer = document.createElement('div');
+//   bottomSpacer.classList.add('height-500');
+//   itemContainer.appendChild(bottomSpacer);
+// }
+
+// Displays 3, 2, 1, GO!
+function countdownStart() {
+  let count = 3;
+  const theCountdown = setInterval(() => {
+    countdown.textContent = count;
+    count--;
+    if (count === -1) {
+      countdown.textContent = 'GO!';
+      clearInterval(theCountdown);
+    }
+  }, 1000);
+}
+
+// Navigate from Splash Page to Countdown Page
+function showCountdown() {
+    countdownPage.hidden = false;
+    splashPage.hidden = true;
+    countdownStart();
+    createEquations();
+}
+
+// Get the value from selected radio button
+function getRadioValue() {
+  let radioValue;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value;
+    }
+  });
+  return radioValue;
+}
+
+// Form that decideds amount of questions
+function selectQuestionAmount(e) {
+  e.preventDefault();
+  getRadioValue();
+  questionAmount = getRadioValue();
+  console.log('question amount:', questionAmount);
+  questionAmount ? showCountdown() : alert('Please Select an Option to Play!');
+}
+
+startForm.addEventListener('click', () => {
+  radioContainers.forEach((radioEl) => {
+    // Remo Select Label Styling
+    radioEl.classList.remove('selected-label');
+    // add it back if the radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
+
+// Event Listeners
+startForm.addEventListener('submit', selectQuestionAmount);
